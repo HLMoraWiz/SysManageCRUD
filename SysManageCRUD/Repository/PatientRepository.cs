9c26f67b20b2390630e53d455f3b27f41ba43d48
Henry <119827170+H-Mora@users.noreply.github.com>
2023-11-02 15:51:52 -0600

    Create read-only property 'IDConnection' and Initialize method in constructor

---
+++ SysManageCRUD/Repository/PatientRepository.cs
@@ -0,0 +1,39 @@
+﻿using SysManageCRUD.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SysManageCRUD.Repository
+{
+    public class PatientRepository : IPatientRepository
+    {
+        private readonly IDbConnection _bd;
+        public PatientRepository(IConfiguration configuration)
+        {
+            _bd = new SqlConnection(configuration.GetConnectionString("ConexionSQLServerDB")); 
+        }
+        public Patient CreatePatient(Patient patient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Patient DeletePatient(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Patient GetPatient(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Patient> GetPatients()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Patient UpdatePatient(Patient patient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
