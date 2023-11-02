97107778c63bd80452783cced7f6982cf661c49b
Henry <119827170+H-Mora@users.noreply.github.com>
2023-11-02 13:59:33 -0600

    Create properties and field required

--- SysManageCRUD/Models/Patient.cs
+++ SysManageCRUD/Models/Patient.cs
@@ -4,7 +4,23 @@ namespace SysManageCRUD.Models
 {
     public class Patient
     {
-     
+        [Key]
+        public int IdPatient { get; set; }
+
+        [Required(ErrorMessage = "Patient name is required")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "field must be at least 6 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Patient lastname is required")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "field must be at least 6 characters")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Patient age is required")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "field must be at least 6 characters")]
+        public string Description { get; set; }
 
     }
 }
