8d5144b458bc3107d83cfeca8978bd9dc6d69241
Henry <119827170+H-Mora@users.noreply.github.com>
2023-11-02 17:03:34 -0600

    Add Scope builder services in the class program.

--- SysManageCRUD/Program.cs
+++ SysManageCRUD/Program.cs
@@ -1,7 +1,11 @@
+using SysManageCRUD.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IPatientRepository, PatientRepository>(); 
 
 var app = builder.Build();
 
