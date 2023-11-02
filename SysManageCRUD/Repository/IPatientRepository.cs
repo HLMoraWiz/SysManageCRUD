acfe77b11c226fb5d48f019a81c994fcc99779d5
Henry <119827170+H-Mora@users.noreply.github.com>
2023-11-02 15:17:51 -0600

    Create method without implementation.

--- SysManageCRUD/Repository/IPatientRepository.cs
+++ SysManageCRUD/Repository/IPatientRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace SysManageCRUD.Repository
+﻿using SysManageCRUD.Models;
+
+namespace SysManageCRUD.Repository
 {
     public interface IPatientRepository
     {
+        Patient GetPatient(int id);
+
+        List<Patient> GetPatients();
+
+        Patient CreatePatient(Patient patient); 
+
+        Patient UpdatePatient(Patient patient);
+
+        Patient DeletePatient(int id);
     }
 }
