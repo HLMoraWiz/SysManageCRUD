3efa08da6ce162233f38abd48b7b0f788f646d7b
Henry <119827170+H-Mora@users.noreply.github.com>
2023-11-02 12:12:42 -0600

    Added folder named controller, Added a class named StartController Added a folder named view inside of the folder area

---
+++ SysManageCRUD/Areas/Front/Controllers/StartController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SysManageCRUD.Models;
+using System.Diagnostics;
+
+namespace SysManageCRUD.Controllers
+{
+    [Area("Front")]
+    public class StartController : Controller
+    {
+        private readonly ILogger<StartController> _logger;
+
+        public StartController(ILogger<StartController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- SysManageCRUD/Program.cs
+++ SysManageCRUD/Program.cs
@@ -18,6 +18,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Front}/{controller=Start}/{action=Index}/{id?}");
 
 app.Run();
