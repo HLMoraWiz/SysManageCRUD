59bf7bd2de38a1d770496b5362b4f8d8989ce384
Henry <119827170+H-Mora@users.noreply.github.com>
2023-11-03 00:54:41 -0600

    Create Patient.js,Create Json and method GetPatients to return Json

--- SysManageCRUD/Areas/Admin/Controllers/PatientController.cs
+++ SysManageCRUD/Areas/Admin/Controllers/PatientController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SysManageCRUD.Repository;
 
 namespace SysManageCRUD.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class PatientController : Controller
     {
+        private readonly IPatientRepository _repoPatient;
+
+        public PatientController(IPatientRepository patientRepository)
+        {
+            _repoPatient = patientRepository;       
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+        #region
+        public IActionResult GetPatients()
+        {
+            return Json(new {data = _repoPatient.GetPatients()});
+        }
+        #endregion
+    
+         
+    
+    
     }
 }
---
+++ SysManageCRUD/wwwroot/js/Patients.js
@@ -0,0 +1,36 @@
+﻿var datatable;
+
+$(document).ready(function () {
+    cargarDatatable();
+});
+function cargarDatatable() {
+    datatable = $("#tblPatients").DataTable({
+        "ajax": {
+            "url": "/admin/patient/GetPatients",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "idPatient", "width": "5%" },
+            { "data": "Name", "width": "40%" },
+            { "data": "LastName", "width": "20%" },
+            { "data": "Age", "width": "20%" },
+            { "data": "Description", "width": "20%" },
+            {
+                "data": "idPatient",
+                "render": function (data) {
+                    return `
+                      <div class="text-center">
+                        <a href="/admin/patient/edit/${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px">
+                         <i class="bi bi-pencil-square"></i> Editar
+                        </a>
+                        &nbsp;
+                        <a onclick=Delete("/admin/patient/DeletePatient/${data}") class="btn btn-danger text-white" style="cursor:pointer; width:100px">
+                         <i class="bi bi-x-square"></i> Delete
+                        </a>
+                      </div>`;
+                }
+            }
+        ]
+    });
+}
