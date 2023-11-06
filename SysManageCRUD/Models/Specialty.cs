7fa7d92896910296a392e0b26a00c03b4d27fa05
Henry <119827170+H-Mora@users.noreply.github.com>
2023-11-06 13:02:29 -0600

    Add properties and data annotations for Specialty model class

--- SysManageCRUD/Models/Specialty.cs
+++ SysManageCRUD/Models/Specialty.cs
@@ -1,7 +1,13 @@
-﻿namespace SysManageCRUD.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SysManageCRUD.Models
 { 
     public class Specialty
     {
-
+        [Key]
+        public int IdSpecialty { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "field must be at least 3 characters")]
+        public string SpecialtyName { get; set; }
     }
 }
