56894a71967d2de00805d32d31007b8f3d83132f
Henry <119827170+H-Mora@users.noreply.github.com>
2023-11-06 13:08:16 -0600

    Create some method without implement.

--- SysManageCRUD/Repository/ISpecialty.cs
+++ SysManageCRUD/Repository/ISpecialty.cs
@@ -1,6 +1,14 @@
-﻿namespace SysManageCRUD.Repository
+﻿using SysManageCRUD.Models;
+
+namespace SysManageCRUD.Repository
 {
     public interface ISpecialty
     {
+        Specialty GetSpecialty(int id);
+        List<Specialty> GetSpecialtyList();
+        Specialty UpdateSpecialty(Specialty specialty);
+        void DeleteSpecialty(int id);
+        Specialty CreateSpecialty(Specialty specialty);
+
     }
 }
