82ab65756cc78228ce953bd3c6486f485d14841f
Henry <119827170+H-Mora@users.noreply.github.com>
2023-11-25 16:52:42 -0600

    Create Interface named ILocationRepository and their methods without implement

---
+++ SysManageCRUD/Areas/Admin/Controllers/LocationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SysManageCRUD.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class LocationController : Controller
+    { 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SysManageCRUD/Repository/ILocationRepository.cs
@@ -0,0 +1,15 @@
+﻿using SysManageCRUD.Models;
+
+namespace SysManageCRUD.Repository
+{
+    public interface ILocationRepository
+    {
+
+       Location GetLocation(int idLocation); // Get one element.
+       List<Location> GetLocations(); //get multiples elements. 
+       Location CreateLocation(Location location); //Create a new location
+       Location UpdateLocation(Location location); //Update a location
+       void DeleteLocation(int idLocation); //Delete a location.
+
+    }
+}
