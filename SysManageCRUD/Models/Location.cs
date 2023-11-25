f10f839a8979217415b385826c56c8be529d8591
Henry <119827170+H-Mora@users.noreply.github.com>
2023-11-25 11:00:33 -0600

    Create model location

---
+++ SysManageCRUD/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SysManageCRUD.Models
+{
+    public class Location
+    {
+        [Key]
+        public int IdLocation { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "field must be at least 6 characters")]
+        public string HospitalName { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "field must be at least 6 characters")]
+        public string Address { get; set; }
+    }
+}
