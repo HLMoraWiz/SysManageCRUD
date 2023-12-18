0dc78c667d40393154f37a4ceee929ebb44b506c
Henry <119827170+H-Mora@users.noreply.github.com>
2023-12-18 12:00:55 -0600

    Add properties to the Appointment model

--- SysManageCRUD/Models/Appointment.cs
+++ SysManageCRUD/Models/Appointment.cs
@@ -1,6 +1,15 @@
-﻿namespace SysManageCRUD.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SysManageCRUD.Models
 {
     public class Appointment
     {
+
+        [Key]
+        public int IdAppointment { get; set; }
+        public DateTime Date { get; set; }
+        public int IdPatient { get; set; }
+        public int IdLocation { get; set; }
+
     }
 }
