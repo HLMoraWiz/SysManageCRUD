d7ce8e3355f712de364c3a2233222e2e6ff8ee52
Henry <119827170+H-Mora@users.noreply.github.com>
2023-12-23 02:09:28 -0600

    Create a model user and add buttons named Access and Exit.

---
+++ SysManageCRUD/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SysManageCRUD.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid User_ID { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} & maximum {1} characters.  ", MinimumLength = 5)]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "This fiel is required")]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} & maximum {1} characters. ", MinimumLength = 6)]
+        public string Password { get; set; }
+
+    }
+}
